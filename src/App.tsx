00bff2148d3561bb0cab085eef76bf04e49c2190
Aleksandr Karpenko <KarpenkoAP@eruditor-group.com>
2024-11-19 14:22:42 +0300

    remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@
 import React from 'react';
 import {MainScreen} from './MainScreen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import {ReanimatedScreenProvider} from 'react-native-screens/reanimated';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -19,21 +18,11 @@ function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
       <ReanimatedScreenProvider>
-        <GestureHandlerRootView>
-          <NavigationContainer>
-            <Stack.Navigator
-              initialRouteName="First"
-              screenOptions={{
-                fullScreenGestureEnabled: true,
-                animation: 'fade_from_bottom',
-                headerLargeTitle: true,
-                // animationMatchesGesture: true,
-                // headerTranslucent: true,
-              }}>
-              <Stack.Screen name="First" component={MainScreen} />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </GestureHandlerRootView>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="First">
+            <Stack.Screen name="First" component={MainScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </ReanimatedScreenProvider>
     </SafeAreaProvider>
   );
